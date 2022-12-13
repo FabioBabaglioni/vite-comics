f90d1078405a57419fe797da9dd9fa612cb99370
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-13 15:47:13 +0100

    Creo card statica

--- src/components/contentMain.vue
+++ src/components/contentMain.vue
@@ -11,26 +11,55 @@ export default {
 
 <template>
     <main>
-        <div>
-            <h1>content goes here</h1>
-        </div>
+        <section class="container">
+            <div class="container_flex">
+                <div class="card" v-for="(card, index) in cards" :key="index">
+                    <div class="box">
+                        <img class="img_small"
+                            src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                            alt="">
+                    </div>
+                    <div class="text_series">{{ card.series }}</div>
+                </div>
+            </div>
+
+        </section>
     </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "./styles/partials/variables" as *;
+
 main {
-    height: 150px;
     background-color: #1c1c1c;
-}
 
-div {
-    width: 70%;
-    margin: 0 auto;
-}
+    .container_flex {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        width: 100%;
 
-h1 {
-    color: white;
-    padding-top: 60px;
-    font-size: 30px;
+        .card {
+            width: calc((100% / 6) - 10px);
+            height: auto;
+            padding: 20px 5px;
+        }
+
+        .box {
+            width: 140px;
+            height: 140px;
+            overflow: hidden;
+        }
+
+        .text_series {
+            color: $brandSecondaryColor;
+            font-size: 15px;
+            padding-top: 20px;
+        }
+    }
+
+
+
+
 }
 </style>
